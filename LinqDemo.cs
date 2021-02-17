54f194e3a5f5d3d82744f694cf5eb6ab2038b5fe
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2021-02-17 10:02:24 +0530

    [Tirupathi] Add. Linq Demo

---
+++ LinqDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KT_Revision
+{
+    class LinqDemo
+    {
+        List<int> listNumbers = new List<int>();
+        public LinqDemo()
+        {
+            listNumbers.Add(2);
+            listNumbers.Add(4);
+            listNumbers.Add(6);
+            listNumbers.Add(3);
+            linqExpression();
+        }
+        public void linqExpression()
+        {
+            // Query Syntax
+            var list = from item in listNumbers
+                       where item < 5 
+                       select item;
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Lambda Syntax
+            var list2 = listNumbers.Where(item => item < 5);
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace KT_Revision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              
+             *   Platform  :- Android, Desktop, PS4, ioS
+             *   Library   :- refers to functions (inbuilt)
+             *   API       :- similar to library including interfaces (logical representation of library) 
+             *   IDE       :- debugging, refactoring, code generation Eg.: Visual Studio
+             *   SDK       :- libraries, apis, ides, documentations . Eg.: Android SDK
+             *   Framework :- foundation platform on which programs can built for specific platforms
+              
+             *   C# :-
+                    -> object oriented programming language
+                    -> windows apps, mobile apps and games
+                    -> by micosoft runs on .net framework
+
+             *   .Net :-
+                    -> 
+
+            
+            */
+
+
+            LinqDemo linq = new LinqDemo();
+
+        }
+    }
+}
